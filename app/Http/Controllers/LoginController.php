47a950ad1217479aa6a34c83b1943d7c96c72c8a
AnwarHossainSR <mahedianwar@gmail.com>
2021-02-25 02:02:31 +0600

    authentication with 4 users

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+
+            $user = User::where('email','=',$request->email)->first();
+
+            if (!$user) {
+                return back()->with('error','email is not recorded');
+            } else {
+                if ($user->type == 'Superadmin') {
+                    $request->session()->put('loggedUser',$user->id);
+                    return \redirect('dashboard/superadmin');
+                } elseif($user->type == 'Admin') {
+                    $request->session()->put('loggedUser',$user->id);
+                    return \redirect('dashboard/admin');
+                }elseif($user->type == 'Author'){
+                    $request->session()->put('loggedUser',$user->id);
+                    return \redirect('dashboard/author');
+                }else{
+                    $request->session()->put('loggedUser',$user->id);
+                    return \redirect('dashboard/user');
+                }
+            }
+
+        }else{
+            return back()->with('error','Password is incorrect');
+        }
+
+    }
+
+    public function superAdminDashboard()
+    {
+        $data = ['loggedUserInfo'=>User::where('id','=',session('loggedUser'))->first()];
+        return view('users.superadmin',$data);
+    }
+    public function adminDashboard()
+    {
+        $data = ['loggedUserInfo'=>User::where('id','=',session('loggedUser'))->first()];
+        return view('users.admin',$data);
+    }
+    public function authorDashboard()
+    {
+        $data = ['loggedUserInfo'=>User::where('id','=',session('loggedUser'))->first()];
+        return view('users.author',$data);
+    }
+    public function userDashboard()
+    {
+        $data = ['loggedUserInfo'=>User::where('id','=',session('loggedUser'))->first()];
+        return view('users.user',$data);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::post('auth/login', [LoginController::class,'login'])->name('login.custom');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'dashboard'], function () {
+        Route::get('/superadmin',[LoginController::class,'superAdminDashboard']);
+        Route::get('/admin',[LoginController::class,'adminDashboard']);
+        Route::get('/author',[LoginController::class,'authorDashboard']);
+        Route::get('/user',[LoginController::class,'userDashboard']);
+    });
+});
