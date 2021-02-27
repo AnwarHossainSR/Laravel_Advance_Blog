e41efc7cf8c54cbb1ccc0db49e4c76c247019d91
sowrov <sowrov.it@gmail.com>
2021-02-28 03:07:36 +0600

    HomeController class imported in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 
 /*
