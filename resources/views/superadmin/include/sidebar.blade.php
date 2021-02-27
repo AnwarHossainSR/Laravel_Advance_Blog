1a412a364c1d9302154416806adaa91740c4677a
AnwarHossainSR <mahedianwar@gmail.com>
2021-02-27 11:54:48 +0600

    Solve problem in login area

--- resources/views/superadmin/include/sidebar.blade.php
+++ resources/views/superadmin/include/sidebar.blade.php
@@ -3,7 +3,7 @@
     <a href="{{-- {{ route('admin.dashboard') }} --}}" class="brand-link">
     <img src="{{ asset('source/back') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
         style="opacity: .8">
-    <span class="brand-text font-weight-light">Admin Name</span>
+    <span class="brand-text font-weight-light">Super Admin</span>
     </a>
 
     <!-- Sidebar -->
@@ -14,27 +14,36 @@
         <img src="{{ asset('source/back') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
     </div>
 
     <!-- Sidebar Menu -->
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
+        <!-- Add icons to the links using the .nav-icon text-primary class
             with font-awesome or any other icon font library -->
-        <li class="nav-item has-treeview {{-- menu-open --}}">
-            <a href="#" class="nav-link active">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <li class="nav-item">
+                <a href="{{ route('superadmin.dashboard') }}" class="nav-link">
+                <i class="nav-icon text-primary fas fa-th"></i>
                 <p>
-                    Category
+                    Dashboard
+                </p>
+                </a>
+            </li>
+        <li class="nav-item has-treeview {{-- menu-open --}}">
+            <a href="#" class="nav-link ">
+
+            <i class="nav-icon text-primary fab fa-affiliatetheme"></i>
+                <p>
+                    Category management
                     <i class="right fas fa-angle-left"></i>
                 </p>
             </a>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
                     <a href="{{-- {{ route('category.manage') }} --}}" class="nav-link {{-- active --}}">
-                    <i class="far fa-circle nav-icon"></i>
+                    <i class="far fa-circle nav-icon text-primary"></i>
                     <p>Manage</p>
                     </a>
                 </li>
@@ -42,16 +51,50 @@
         </li>
         <li class="nav-item has-treeview {{-- menu-open --}}">
             <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <i class="nav-icon text-primary fab fa-usps text-primary"></i>
                 <p>
-                    Post
+                    Post management
                     <i class="right fas fa-angle-left"></i>
                 </p>
             </a>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
                     <a href="{{-- {{ route('post.index') }} --}}" class="nav-link {{-- active --}}">
-                    <i class="far fa-circle nav-icon"></i>
+                    <i class="far fa-circle nav-icon text-primary"></i>
+                    <p>Manage</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item has-treeview {{-- menu-open --}}">
+            <a href="#" class="nav-link">
+            <i class="nav-icon text-primary fas fa-users"></i>
+                <p>
+                    User management
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{-- {{ route('post.index') }} --}}" class="nav-link {{-- active --}}">
+                    <i class="far fa-circle nav-icon text-primary"></i>
+                    <p>Manage</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item has-treeview {{-- menu-open --}}">
+            <a href="#" class="nav-link">
+            <i class="nav-icon text-primary fas fa-address-book"></i>
+                <p>
+                    Contact
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{-- {{ route('post.index') }} --}}" class="nav-link {{-- active --}}">
+                    <i class="far fa-circle nav-icon text-primary"></i>
                     <p>Manage</p>
                     </a>
                 </li>
@@ -59,13 +102,41 @@
         </li>
         <li class="nav-item">
             <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-th"></i>
+            <i class="nav-icon text-primary fas fa-phone-volume"></i>
             <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
+                Video Call
+               {{--  <span class="right badge badge-danger">New</span> --}}
             </p>
             </a>
         </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="nav-icon text-primary fas fa-comments"></i>
+            <p>
+               Chat Box
+               {{--  <span class="right badge badge-danger">New</span> --}}
+            </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="nav-icon text-primary fas fa-envelope"></i>
+            <p>
+                Emailing
+               {{--  <span class="right badge badge-danger">New</span> --}}
+            </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="nav-icon text-primary fas fa-user-circle"></i>
+            <p>
+                Accounts
+               {{--  <span class="right badge badge-danger">New</span> --}}
+            </p>
+            </a>
+        </li>
+
         </ul>
     </nav>
     <!-- /.sidebar-menu -->
--- resources/views/superadmin/master.blade.php
+++ resources/views/superadmin/master.blade.php
@@ -36,24 +36,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        {{-- <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Management</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard</li>
-                </ol>
-            </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-        </div> --}}
-        <!-- /.content-header -->
-
         <!-- Main content -->
         @yield('content')
         <!-- /.content -->
