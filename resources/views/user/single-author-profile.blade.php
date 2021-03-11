cc07537d18bef890ba77cedd5674356dcd65564b
shaakilahammed <shaakilahammed@gmail.com>
2021-03-12 00:48:38 +0600

    User can Author's profile view

--- resources/views/user/single-author-profile.blade.php
+++ resources/views/user/single-author-profile.blade.php
@@ -7,7 +7,39 @@
 @section('content')
 	<section class="blog-area section">
 		<div class="container">
-
+            <div class="row">
+                <div class="col-md-12">
+      
+                  <!-- Profile Image -->
+                  <div class="card card-primary card-outline">
+                    <div class="card-body box-profile">
+                        <div class="text-center">
+                            <a href="#" class="avater"><img style="max-width: 200px; border-radius:50%" class="" src="{{asset('source/back/profile')}}/{{ $author->profileImage }}" alt="User profile picture"></a>
+                        </div>
+      
+                        <h3 class="profile-username text-center">{{ $author->name }}</h3>
+        
+                        <p class="text-muted text-center">{{ $author->email }}</p>
+        
+                        <ul class="list-group list-group-unbordered mb-3">
+                            {{-- <li class="list-group-item">
+                            <b>Total Post</b> <a class="float-right">{{ count($author->posts) }}</a>
+                            </li> --}}
+                            <li class="list-group-item">
+                            <b>Total Comment</b> <a class="float-right">543</a>
+                            </li>
+                            {{-- <li class="list-group-item">
+                            <b>Joined</b> <a class="float-right">{{ Auth::user()->created_at->toFormattedDateString() }}</a>
+                            </li> --}}
+                        </ul>
+      
+                        <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+            </div>
 			<div class="row">
 				<div class="col-lg-6 col-md-12">
 					@foreach($posts as $key => $post)
