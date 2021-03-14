8b620a514a89a30301b03a04870e7179d565b986
shaakilahammed <shaakilahammed@gmail.com>
2021-03-14 12:45:43 +0600

    User dashboard created

---
+++ resources/views/user/layouts/include/dashboard-sidebar.blade.php
@@ -0,0 +1,104 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="{{ route('user.dashboard') }}" class="brand-link">
+    <img src="{{ asset('source/back') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        style="opacity: .8">
+    <span class="brand-text font-weight-light">User</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+        <img src="{{ asset('source/back/profile') }}/{{ Auth::user()->profileImage }}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+        <a href="{{ route('user.profile') }}" class="d-block">{{ Auth::user()->name }}</a>
+        </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon text-primary class
+            with font-awesome or any other icon font library -->
+            <li class="nav-item">
+                <a href="{{ route('admin.dashboard') }}" class="nav-link {{ (request()->is('dashboard/admin')) ? 'active': '' }}">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                    Dashboard
+                </p>
+                </a>
+            </li>
+        <li class="nav-item has-treeview {{-- menu-open --}}">
+            <a href="{{ route('admin.category.all') }}" class="nav-link {{ (request()->is('admin/category*')) ? 'active': '' }}">
+
+            <i class="nav-icon fas fa-shopping-basket"></i>
+                <p>
+                    Category management
+                </p>
+            </a>
+        </li>
+        <li class="nav-item has-treeview {{-- menu-open --}}">
+            <a href="{{ route('admin.tags.all') }}" class="nav-link {{ (request()->is('admin/tag*')) ? 'active': '' }}">
+
+            <i class="nav-icon fas fa-tags"></i>
+                <p>
+                    Tag Management
+                </p>
+            </a>
+        </li>
+        <li class="nav-item has-treeview {{-- menu-open --}}">
+            <a href="#" class="nav-link ">
+
+            <i class="nav-icon fas fa-clipboard"></i>
+                <p>
+                    Post Management
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{ route('admin.posts.all') }}" class="nav-link {{ (request()->is('admin/posts*')) ? 'active': '' }}">
+                    <i class="fas fa-tasks nav-icon "></i>
+                    <p>Manage Post</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.posts.pending') }}" class="nav-link {{ (request()->is('admin/post/pending*')) ? 'active': '' }}">
+                    <i class="fab fa-twitch nav-icon "></i>
+                    <p>Pending Post</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('admin.posts.own') }}" class="nav-link {{ (request()->is('admin/posts/own*')) ? 'active': '' }}">
+                    <i class="fas fa-user-shield nav-icon "></i>
+                    <p>Admin Post</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-item has-treeview {{-- menu-open --}}">
+            <a href="{{ route('admin.profile') }}" class="nav-link {{ (request()->is('admin/profile*')) ? 'active': '' }}">
+
+            <i class="nav-icon fas fa-user"></i>
+                <p>
+                    Profile
+                </p>
+            </a>
+        </li>
+
+
+
+
+
+
+
+
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
---
+++ resources/views/user/user-dashboard.blade.php
@@ -0,0 +1,156 @@
+@extends('user.layouts.dashboard-master')
+
+@section('title')
+    User || Dashboard
+@endsection
+
+@section('content')
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Dashboard</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- Main content -->
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-info">
+                <div class="inner">
+                  <h3>{{ $post }}</h3>
+
+                  <p>Posts</p>
+                </div>
+                <div class="icon">
+                  <i class="fas fa-pen-square"></i>
+                </div>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3>{{ $category }}</h3>
+
+                  <p>Categories</p>
+                </div>
+                <div class="icon">
+                  <i class="fas fa-tags"></i>
+                </div>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-warning">
+                <div class="inner">
+                  <h3>{{ $tag }}</h3>
+
+                  <p>Tags</p>
+                </div>
+                <div class="icon">
+                  <i class="fas fa-tag"></i>
+                </div>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-danger">
+                <div class="inner">
+                  <h3>{{ $user }}</h3>
+
+                  <p>Users</p>
+                </div>
+                <div class="icon">
+                  <i class="fas fa-user"></i>
+                </div>
+              </div>
+            </div>
+            <!-- ./col -->
+          </div>
+          {{-- <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h3 class="card-title">Post List</h3>
+                            <a href="#" class="btn btn-primary">Post List</a>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body p-0">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th style="width: 10px">#</th>
+                                    <th>Image</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Tags</th>
+                                    <th>Author</th>
+                                    <th>Created Date</th>
+                                    <th style="width: 40px">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if($posts->count())
+                                @foreach ($posts as $post)
+                                    <tr>
+                                        <td>{{ $post->id }}</td>
+                                        <td>
+                                            <div style="max-width: 70px; max-height:70px;overflow:hidden">
+                                                <img src="{{ asset($post->image) }}" class="img-fluid img-rounded" alt="">
+                                            </div>
+                                        </td>
+                                        <td>{{ $post->title }}</td>
+                                        <td>{{ $post->category->name }}</td>
+                                        <td>
+                                            @foreach($post->tags as $tag)
+                                                <span class="badge badge-primary">{{ $tag->name }} </span>
+                                            @endforeach
+                                        </td>
+                                        <td>{{ $post->user->name }}</td>
+                                        <td>{{ $post->created_at->format('d M, Y') }}</td>
+                                        <td class="d-flex">
+                                            <a href="{{ route('post.show', [$post->id]) }}" class="btn btn-sm btn-success mr-1"> <i class="fas fa-eye"></i> </a>
+                                            <a href="{{ route('post.edit', [$post->id]) }}" class="btn btn-sm btn-primary mr-1"> <i class="fas fa-edit"></i> </a>
+                                            <form action="{{ route('post.destroy', [$post->id]) }}" class="mr-1" method="POST">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-danger"> <i class="fas fa-trash"></i> </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="6">
+                                            <h5 class="text-center">No posts found.</h5>
+                                        </td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+            </div>
+        </div> --}}
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+
+@endsection
